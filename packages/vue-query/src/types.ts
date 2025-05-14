3501fc3eb9e9d4ebe678200e92b22a246244d860
unnoq <contact@unnoq.com>
2025-05-14 14:26:59 +0700

    wip

--- packages/vue-query/src/types.ts
+++ packages/vue-query/src/types.ts
@@ -60,7 +60,7 @@ export type SkipableInfiniteOptionsIn<TClientContext extends ClientContext, TInp
     MaybeRefDeep<InfiniteQueryObserverOptions<TOutput, TError, TSelectData, TOutput, QueryKey, TPageParam>[Property]>;
   }
   & {
-    input: (pageParam: TPageParam) => MaybeRefDeep<TInput>
+    input: (pageParam: TPageParam) => MaybeRefDeep<TInput | SkipToken>
     enabled?: MaybeRefOrGetter<InfiniteQueryObserverOptions<TOutput, TError, TSelectData, TOutput, QueryKey, TPageParam>['enabled']>
     queryKey?: MaybeRefDeep<InfiniteQueryObserverOptions<TOutput, TError, TSelectData, TOutput, QueryKey, TPageParam>['queryKey']>
     shallow?: boolean
